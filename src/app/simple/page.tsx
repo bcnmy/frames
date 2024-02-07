610a0703cdab10576f043ea91903a5d04fb8fdc1
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-02-07 09:51:06 +0100

    small test

--- src/app/simple/page.tsx
+++ src/app/simple/page.tsx
@@ -38,6 +38,7 @@ export default async function Page(props: {
         <FrameImage src={CONSTANTS.IMAGE_URL} />
         <FrameButton href={CONSTANTS.DASHBOARD_URL}>📊 Dashboard</FrameButton>
         <FrameButton href={CONSTANTS.DOCS_URL}>📄 Docs</FrameButton>
+        <FrameButton href={`https://www.google.com`}>External</FrameButton>
       </FrameContainer>
     </>
   );
