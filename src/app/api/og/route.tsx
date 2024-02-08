a23044cadd8108daea07c66441b32650401caa1c
himanshugarg06 <garg91824@gmail.com>
2024-02-08 14:41:04 +0530

    remove any

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -60,7 +60,7 @@ export async function GET(request: Request) {
         height: 630,
       },
     );
-  } catch (e: any) {
+  } catch (e) {
     return new Response(`Failed to generate the image`, {
       status: 500,
     });
